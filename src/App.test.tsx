3d9a320719281000c3f2473548669da3ba94e9ff
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2021-04-16 13:54:28 +0200

    test: update one unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import { render/*, screen*/ } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+
+// https://www.freecodecamp.org/news/testing-react-hooks/
+// https://www.algolia.com/ref/docsearch/?utm_source=docsearch&utm_medium=link&utm_term=footer&utm_campaign=
 
 test('renders learn react link', () => {
     render(<App />);
-    // const linkElement = null;//screen.findAllByTestId('agenda-container');
-    // expect(linkElement).toBeInTheDocument();
-    expect(true).toBe(true);
+
+    const appContainer = screen.getByTestId('app-container');
+
+    expect(appContainer).toBeInTheDocument();
 });
