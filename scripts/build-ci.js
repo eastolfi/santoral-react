58958c0ed00e7fedd8edbc95813148816abd27bb
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2022-08-02 11:51:07 +0200

    chore: improve build logs

--- scripts/build-ci.js
+++ scripts/build-ci.js
@@ -1,7 +1,11 @@
 const { exec } = require('child_process');
 
 if (process.env.BUILD_TYPE === 'API') {
+    console.log('Running npm run build:server');
     exec('npm run build:server');
 } else {
+    console.log('Running npm run build:front');
     exec('npm run build:front');
 }
+
+console.log('Build done');
