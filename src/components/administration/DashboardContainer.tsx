b49e49143244e73157d182f8469de01a43f81e6a
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2022-08-02 09:46:44 +0200

    feat(admin): add a new settings page

--- src/components/administration/DashboardContainer.tsx
+++ src/components/administration/DashboardContainer.tsx
@@ -7,14 +7,18 @@ import './DashboardContainer.css';
 
 export function DashboardContainer() {
     return (
-        <Grid container>
+        <Grid container spacing={2}>
             <Grid item xs={2}>
                 <DashboardItem link="/admin/events">
-                    <Typography variant="h6">
-                        Events
-                    </Typography>
+                    <Typography variant="h6">Events</Typography>
+                </DashboardItem>
+            </Grid>
+
+            <Grid item xs={2}>
+                <DashboardItem link="/admin/settings">
+                    <Typography variant="h6">Settings</Typography>
                 </DashboardItem>
             </Grid>
         </Grid>
-    )
+    );
 }
---
+++ src/pages/admininstration/settings/SettingsAdminPage.tsx
@@ -0,0 +1,11 @@
+import { DashboardItemPage } from '../../../components/administration/DashboardItemPage';
+import { SettingsAdminContainer } from '../../../components/administration/settings/SettingsAdminContainer';
+
+export function SettingsAdminPage() {
+    return (
+        <DashboardItemPage>
+            <SettingsAdminContainer />
+        </DashboardItemPage>
+    )
+}
+
